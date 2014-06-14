9496199cdbefaaa3decb74cf9008e1a29ed0c29c
Christian Haller <chrisitan.haller@globetrotter.de>
2014-06-14 16:09:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    $response = '';
+    if (isset($_GET['url']) && $_GET['url'] !== ''){
+    $url = $_GET['url'];
+    include('parse.php');
+}?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="/css/jquery-jvectormap-1.2.2.css"/>
+
+    <title><?php echo $page->title;?></title>
+</head>
+<body>
+    <?php if($response!==''):?>
+    <?php echo $response;?>
+        <h1><?php echo $page->title;?></h1>
+    <div id="world-map"></div>
+    <textarea><?php echo $json;?></textarea>
+
+    <?php else: ?>
+<h1>Give me your Tridadvisor map</h1>
+<form action="">
+    <input value="" name="url" type="text"/>
+</form>
+ <?php endif;?>
+
+<script src="/js/jquery-1.11.1.min.js"></script>
+<script src="/js/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="/js/jquery-jvectormap-world-mill-en.js"></script>
+<script src="/js/custom.js"></script>
+</body>
+</html>
+
+
+
---
+++ parse.php
@@ -0,0 +1,118 @@
+<?php
+
+include('iso.php');
+
+
+
+
+$context = stream_context_create( array(
+  'http'=>array(
+    'timeout' => 2.0
+  )
+));
+
+$urlParts = parse_url($url);
+
+if($urlParts['host']!=='www.tripadvisor.com'){
+    $error = 'invalid url';
+    die($error);
+}
+
+
+// Datei runterladen, könnte schief gehen
+$handle = fopen($url, "r",false,$context);
+if ( !$handle ) {
+    $error = 'can reach url';
+    die($error);
+}
+
+
+$str = '';
+while (!feof($handle)) {
+    $line_of_text = fgets($handle);
+    $str .= $line_of_text;
+}
+fclose($handle);
+
+
+
+// Name parsen
+$startNameString = '<div class="memberTitle">';
+$startNamePosition = strpos($str,$startNameString);
+$username = substr($str, $startNamePosition+strlen($startNameString));
+$endNamePosition = strpos($username,'<');
+$username = substr($username, 0, $endNamePosition);
+$page = new stdClass();
+$page->title = 'Hi '.$username.' this is your map';
+
+
+// Pins parsen
+$start = strpos($str, 'data =');
+$str = substr($str, $start + 7);
+$end = strpos($str, ',"modules.membercenter.model.FriendCount"');
+$str = substr($str, 0, $end - 1);
+$str .= '}}}';
+
+
+// könnte schief gehen
+$json_a = json_decode($str, true);
+if ($json_a === NULL) {
+    $error = 'parse error';
+    die($error);
+
+}
+
+
+$places = array();
+
+$isoArray = array_flip($iso);
+
+
+
+foreach ($json_a['store']['modules.unimplemented.entity.LightWeightPin'] as $pin) {
+    $locationArray = explode(',', $pin['name']);
+    $country = ltrim($locationArray[sizeof($locationArray) - 1]);
+    $city = $locationArray[0];
+    $iso = $isoArray[$country];
+
+
+
+
+    try {
+        if ($iso === null) {
+            $iso = '';
+            throw new Exception($country . ' not in ISO' . "\n");
+
+        }
+
+    } catch (Exception $e) {
+        //echo $e->getMessage();
+
+    }
+
+
+    $places[] =
+        [
+            'country' => $country,
+            'name' => $pin['name'],
+            'city' => $city,
+            'lat' => $pin['lat'],
+            'lng' => $pin['lng'],
+            'flags' => $pin['flags'],
+            'iso' => $iso
+        ];
+
+};
+$json = json_encode($places);
+$url = urlencode($url);
+
+$path = 'data/'.$url;
+if(!is_dir($path)){
+    mkdir($path);
+}
+
+file_put_contents('data/'.$url.'/'.time(),$json);
+
+$response =  '<script> var data = '.$json.'</script>';
+
+?>
