9446de2919c59a91017e66860038812a529b6e3e
Christian Haller <chrisitan.haller@globetrotter.de>
2014-06-18 00:22:30 +0200

    git pull

--- github-hook.php
+++ github-hook.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 //if ( $_POST['payload'] ) {
-shell_exec( 'cd /var/www/christianhaller.com/subdomains/download-your-travelmap/ && git reset --hard HEAD && git pull' );
+shell_exec( 'cd /var/www/christianhaller.com/subdomains/download-your-travelmap/ && git pull origin master' );
 //}
 
 ?>hi
