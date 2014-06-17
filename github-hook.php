5e5e8bd895efefb8e5f9e6cc613ec2c7ce901157
Christian Haller <chrisitan.haller@globetrotter.de>
2014-06-18 01:16:16 +0200

    hook

--- github-hook.php
+++ github-hook.php
@@ -5,18 +5,6 @@
 //if ( $_POST['payload'] ) {
 echo shell_exec("cd /var/www/christianhaller.com/subdomains/download-your-travelmap && /usr/bin/git pull 2>&1");
 
-
-
-
-if(function_exists('exec')) {
-    echo "exec is enabled";
-}
-
-else {
-    echo 'disabled';
-}
 //}
 
 ?>hi
-
-<?php phpinfo();?>
