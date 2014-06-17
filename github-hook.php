774d5c69aaf0664fecced77f2ac18146becb2b00
Christian Haller <chrisitan.haller@globetrotter.de>
2014-06-18 00:41:53 +0200

    exec

--- github-hook.php
+++ github-hook.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 //if ( $_POST['payload'] ) {
-echo shell_exec("/usr/bin/git pull 2>&1");
+echo shell_exec("cd /var/www/christianhaller.com/subdomains/download-your-travelmap && /usr/bin/git pull 2>&1");
 
 
 
