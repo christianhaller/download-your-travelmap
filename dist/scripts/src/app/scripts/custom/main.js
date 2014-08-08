e396d1f88c6750db88528930c17fab51b57cd6ef
Haller, Christian <haller.christian@guj.de>
2014-08-08 14:39:43 +0200

    validation

---
+++ dist/scripts/src/app/scripts/custom/main.js
@@ -0,0 +1,12 @@
+/*global ga,Tc */
+(function($) {
+    'use strict';
+    $(document).ready(function() {
+        var $page = $(document.body),
+            application = new Tc.Application($page);
+        application.registerModules();
+        application.start();
+    });
+    ga('create', 'UA-53443219-1', 'auto');
+    ga('send', 'pageview');
+})(Tc.$);
