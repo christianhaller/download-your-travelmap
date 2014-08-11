fe1c3b1c75a2cb6c6ebee9d4c477649cb37a28aa
Haller, Christian <haller.christian@guj.de>
2014-08-11 14:13:11 +0200

    inline

---
+++ dist/scripts/src/app/scripts/custom/tracking/tracking.js
@@ -0,0 +1,13 @@
+/* global Tc,ga */
+(function($) {
+    'use strict';
+    Tc.Module.Tracking = Tc.Module.extend({
+
+        on: function(callback) {
+            callback();
+        },
+        onTrack: function(data) {
+            ga(data.data[0], data.data[1], data.data[2], data.data[3],data.data[4]);
+        }
+    });
+})(Tc.$);
