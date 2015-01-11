e716ec92e8fc76a044786785b71ca082ad46c8ba
Haller, Christian <haller.christian@guj.de>
2015-01-11 15:06:15 +0100

    google webfont loader

--- app/scripts/custom/main.js
+++ app/scripts/custom/main.js
@@ -39,3 +39,15 @@
         window.onerror = trackJavaScriptError;
     }
 })();
+
+var WebFontConfig = {
+    google: { families: [ 'Roboto:500:latin' ] }
+};
+(function(){
+    'use strict';
+    var wf = document.createElement("script");
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.async = 'true';
+    document.head.appendChild(wf);
+})();
