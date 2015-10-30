86af660a65cd42a7aa92875b8ff8081a252ba4bc
Christian Haller <hallo@christianhaller.com>
2015-10-30 22:50:33 +0100

    travis

--- gulp/tasks/svgsprite.js
+++ gulp/tasks/svgsprite.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
-   svgSprite = require("gulp-svg-sprites");
+   svgSprite = require('gulp-svg-sprites');
 
 gulp.task('svgSprite', function () {
     return gulp.src('app/svg/*.svg')
-        .pipe(svgSprite({ mode: "defs",preview: false}))
-        .pipe(gulp.dest("build"));
+        .pipe(svgSprite({ mode: 'defs',preview: false}))
+        .pipe(gulp.dest('build'));
 });
