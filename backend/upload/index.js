ca751be1e0fd6125b337c07b79dd39ce50dbabd5
Christian Haller <hallo@christianhaller.com>
2015-11-05 00:00:05 +0100

    process.env

--- backend/upload/index.js
+++ backend/upload/index.js
@@ -7,10 +7,11 @@ module.exports = function (dir, filename, content, contentType, contentEncoding)
     filename = dir + '/' + sanitize(filename);
     //AWS.config.loadFromPath('./aws.json');
 
-    console.log(process.env.Key);
-    if(typeof process.env.Key !== 'undefined' && typeof process.env.Secret !== 'undefined'){
+    console.log('env');
+    console.log(process.env.AccessKey);
+    if(typeof process.env.AccessKey !== 'undefined' && typeof process.env.Secret !== 'undefined'){
         console.log('Eeeeeeee');
-        AWS.config.accessKeyId = process.env.Key;
+        AWS.config.accessKeyId = process.env.AccessKey;
         AWS.config.secretAccessKey = process.env.Secret;
         AWS.config.region = 'us-east-1';
     }
