56ec231f49b0df3830c2b90526ee294eec74cc33
Christian Haller <hallo@christianhaller.com>
2015-11-10 00:51:03 +0100

    test

--- test/index.js
+++ test/index.js
@@ -20,7 +20,7 @@ describe('app', function () {
 
 
     it('Be OK https://en.wikipedia.org/wiki/Be_OK_%28Ingrid_Michaelson_song%29', function (done) {
-        this.timeout(6000);
+        this.timeout(9000);
         app.handler({
             'url': decodeURIComponent('http://www.tripadvisor.com/members/christianhaller')
         }, {
