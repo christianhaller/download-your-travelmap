bfc41ea2bf6dce3c9479a89d1693ffc0acf931f2
Christian Haller <hallo@christianhaller.com>
2015-11-13 00:55:56 +0100

    stage test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@
 (function () {
     'use strict';
     var app = require('../backend'),
-        request = require('../backend/request'),
+        requestApp = require('../backend/request'),
+        https = require('https'),
         xml2js = require('xml2js/'),
         parser = new xml2js.Parser(),
         expect = require('../backend/node_modules/expect.js/'),
@@ -11,14 +12,32 @@
     describe('redirect', function () {
         it('301', function (done) {
             this.timeout(9000);
-            request('http://www.tripadvisor.co.uk/MemberProfile-a_uid.F3B46B68117496775EE93A2AB6A9C1DC').then(function (data) {
+            requestApp('http://www.tripadvisor.co.uk/MemberProfile-a_uid.F3B46B68117496775EE93A2AB6A9C1DC').then(function (data) {
                 expect(data.username).to.equal('Atanas_GK');
                 done();
             });
         });
     });
 
+    describe('stage okay', function(){
+        it('gateway should send json', function(done){
+            this.timeout(9000);
+            https
+                .get('https://eucch0w5pe.execute-api.us-east-1.amazonaws.com/prod?url=http%3A%2F%2Fwww.tripadvisor.com%2Fmembers%2FCarolinaCoopers',function (res){
+                    var body = '';
+                    expect(res.statusCode).to.equal(200);
 
+                    res.on('data', function(chunk) {
+                        body += chunk;
+                    });
+                    res.on('end', function() {
+                        expect(JSON.parse(body).data.username).to.equal('CarolinaCoopers');
+                        done()
+                    });
+
+                });
+        });
+    });
 
     describe('app', function () {
         it('Be OK https://en.wikipedia.org/wiki/Be_OK_%28Ingrid_Michaelson_song%29', function (done) {
@@ -58,7 +77,7 @@
 
         it('404', function (done) {
             this.timeout(4000);
-            request(' http://www.tripadvisor.com/christianhaller').catch(function (err) {
+            requestApp(' http://www.tripadvisor.com/christianhaller').catch(function (err) {
                 expect(err).to.equal('profile not found');
                 done();
             });
@@ -67,7 +86,7 @@
 
         it('map request', function (done) {
             this.timeout(6000);
-            request(' http://www.tripadvisor.com/TravelMap-a_uid.BAE86B9F2C0155C5003524F652DD4719').then(function (data) {
+            requestApp(' http://www.tripadvisor.com/TravelMap-a_uid.BAE86B9F2C0155C5003524F652DD4719').then(function (data) {
                 expect(data.username).to.equal('surefire56');
                 done();
             }).catch(function (err) {
@@ -77,7 +96,7 @@
 
         it('wrong url', function (done) {
             this.timeout(4000);
-            request('http://www.tripadvisor.com').catch(function (err) {
+            requestApp('http://www.tripadvisor.com').catch(function (err) {
                 expect(err).to.be.an(Error);
                 done();
             });
@@ -85,7 +104,7 @@
 
         it('no url', function (done) {
             this.timeout(4000);
-            request('').catch(function (err) {
+            requestApp('').catch(function (err) {
                 expect(err).to.be.an(Error);
                 done();
             });
@@ -93,7 +112,7 @@
 
         it('bad url', function (done) {
             this.timeout(4000);
-            request('http://www.google.com').catch(function (err) {
+            requestApp('http://www.google.com').catch(function (err) {
                 expect(err).to.be.an(Error);
                 done();
             });
@@ -101,7 +120,7 @@
 
         it('dns error', function (done) {
             this.timeout(4000);
-            request('http://www.google.commmmm').catch(function (err) {
+            requestApp('http://www.google.commmmm').catch(function (err) {
                 expect(err).to.be.an(Error);
                 done();
             });
