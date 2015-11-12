8c881788f0f7b20707b89c70689d18c3ff81a381
Haller, Christian <haller.christian@guj.de>
2015-11-12 14:37:22 +0100

    change

--- backend/upload/index.js
+++ backend/upload/index.js
@@ -3,20 +3,10 @@
     'use strict';
     var AWS = require('aws-sdk'),
         sanitize = require('sanitize-filename'),
-        Promise = require('promise');
+        Promise = require('promise'),
+        config = require('../../config.json');
 
     module.exports = function (dir, filename, content, contentType, contentEncoding) {
-        // TODO merge config /Users/christianhaller/Desktop/download-your-travelmap/config.json
-        var config = {
-            "aws": {
-                "s3": {
-                    // /Users/christianhaller/Desktop/download-your-travelmap/backend/upload/index.js
-                    "bucketName": "download-your-travelmap.christianhaller.com",
-                    "region": "us-west-2"
-                }
-
-            }
-        };
         filename = dir + '/' + sanitize(filename);
 
         AWS.config.region = config.aws.s3.region;
