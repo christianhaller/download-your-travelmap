e6d0d9cf39d29c785098d2f73a769a4051c3c37c
Christian Haller <hallo@christianhaller.com>
2015-11-19 22:02:30 +0100

    js errors

--- test/casperjs/index.js
+++ test/casperjs/index.js
@@ -2,7 +2,8 @@
 (function (casper) {
     'use strict';
     var tripAdvisorProfileUrl = 'http://www.tripadvisor.com/members/christianhaller',
-        stageUrl = 'http://stage.download-your-travelmap.christianhaller.com/';
+        stageUrl = 'http://stage.download-your-travelmap.christianhaller.com/',
+        jsErrors = [];
 
     casper.options.viewportSize = {width: 1600, height: 950};
     casper.on("resource.error", function (resourceError) {
@@ -10,22 +11,29 @@
         console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
     });
 
+    casper.on("page.error", function (msg, trace) {
+        this.echo('Error:    ' + msg, 'ERROR');
+        this.echo('file:     ' + trace[0].file, 'WARNING');
+        this.echo('line:     ' + trace[0].line, 'WARNING');
+        this.echo('function: ' + trace[0]['function'], 'WARNING');
+        jsErrors.push(msg);
+    });
 
     casper.test.begin('basic functions', function (test) {
 
 
         casper.start(stageUrl, function () {
 
-            test.assertResourceExists(function(resource) {
+            test.assertResourceExists(function (resource) {
                 return resource.url.match('http://www.google-analytics.com/r/collect');
-            },'Google Analytics');
+            }, 'Google Analytics');
 
 
-            this.fill('.url-form', { url: 'http://www.google.com' }, true);
-            test.assertExists('#url.error','form validation');
+            this.fill('.url-form', {url: 'http://www.google.com'}, true);
+            test.assertExists('#url.error', 'form validation');
 
 
-            this.fill('.url-form', { url: tripAdvisorProfileUrl }, true);
+            this.fill('.url-form', {url: tripAdvisorProfileUrl}, true);
 
             this.waitForUrl(stageUrl + '?url=' + tripAdvisorProfileUrl, function () {
                 console.log(this.getCurrentUrl());
@@ -35,9 +43,13 @@
         });
 
 
-
-
         casper.run(function () {
+            if (jsErrors.length > 0) {
+                this.echo(jsErors.length + ' Javascript errors found', 'WARNING');
+            } else {
+                this.echo(jsErrors.length + ' Javascript errors found', 'INFO');
+            }
+
             test.done();
         });
     });
