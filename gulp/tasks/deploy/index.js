1f3710e24158e5bc08a6ac817f8d879c7b46430a
Haller, Christian <haller.christian@guj.de>
2015-11-24 17:38:32 +0100

    cache header

--- gulp/tasks/deploy/index.js
+++ gulp/tasks/deploy/index.js
@@ -6,26 +6,26 @@
         awspublish = require("gulp-awspublish"),
         zip = require('gulp-zip'),
         config = require('../../../backend/config.json'),
-    deploy = function (config) {
-        var publisher = awspublish.create({
-                region: config.s3.region,
-                'params': {
-                    Bucket: config.s3.bucketName
-                }
-            }),
-            headers = {
-                'Cache-Control': 'max-age=315360000, no-transform, public',
-                'Content-Encoding': 'gzip'
-            };
-        gulp.src('./backend/**/*')
-            .pipe(zip('archive.zip'))
-            .pipe(lambda(config.lambda, config));
+        deploy = function (config) {
+            var publisher = awspublish.create({
+                    region: config.s3.region,
+                    'params': {
+                        Bucket: config.s3.bucketName
+                    }
+                }),
+                headers = {
+                    'Cache-Control': 'max-age=3600, no-transform, public, must-revalidate',
+                    'Content-Encoding': 'gzip'
+                };
+            gulp.src('./backend/**/*')
+                .pipe(zip('archive.zip'))
+                .pipe(lambda(config.lambda, config));
 
-        return gulp.src(['./dist/' + config.filename, './app/robots.txt'])
-            .pipe(awspublish.gzip({}))
-            .pipe(publisher.publish(headers))
-            .pipe(awspublish.reporter({}));
-    };
+            return gulp.src(['./dist/' + config.filename, './app/robots.txt'])
+                .pipe(awspublish.gzip({}))
+                .pipe(publisher.publish(headers))
+                .pipe(awspublish.reporter({}));
+        };
 
     gulp.task('deploy_prod', ['default'], function () {
         config = config.aws.prod;
