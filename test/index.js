4336ffe7192b8960be9d989cc3c4a937fb9fdce8
Christian Haller <hallo@christianhaller.com>
2016-02-09 00:23:07 +0100

    slimerjs

--- test/index.js
+++ test/index.js
@@ -3,9 +3,14 @@
     'use strict';
     var childProcess = require('child_process'),
         ls,
+        host = 'http://stage.download-your-travelmap.christianhaller.com/',
         failure = [];
 
-    ls = childProcess.exec('casperjs test test/test.js --jsconsole --engine=slimerjs --url=http://localhost:8080 --web-security=no --ssl-protocol=any --ignore-ssl-errors=yes');
+    if(process.argv[2] === 'local'){
+        host = 'http://localhost:8080'
+    }
+
+    ls = childProcess.exec('casperjs test test/test.js --ignore-ssl-errors=true --ssl-protocol=any --engine=slimerjs --url=' + host);
     ls.stdout.on('data', function (data) {
         console.log(data);
         if (data.indexOf('FAIL') === 10) {
