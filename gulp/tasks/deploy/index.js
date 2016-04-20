a77b61cb67aa4c2e2cc276e8b1b2c2b7e12f1113
Christian Haller <hallo@christianhaller.com>
2016-04-20 21:47:57 +0200

    finally fixed

--- gulp/tasks/deploy/index.js
+++ gulp/tasks/deploy/index.js
@@ -32,7 +32,7 @@
                 }))
                 .pipe(awspublish.reporter({}));
 
-            return gulp.src(['./dist/prod1.html'])
+            return gulp.src(['./dist/index.html'])
                 .pipe(zopfli({'append': false}))
 
                 .pipe(publisher.publish(headerIndex, {
