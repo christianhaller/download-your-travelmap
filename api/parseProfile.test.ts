46c0ea6db22fb9d080c2df90e2f05457f991de3e
Christian <hallo@christianhaller.com>
2020-07-05 23:34:23 +0200

    parse profile

--- api/parseProfile.test.ts
+++ api/parseProfile.test.ts
@@ -4,8 +4,8 @@ import {
     assertEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
-Deno.test("parseMap", async () => {
+Deno.test("parseProfile", async () => {
     const str = await readFileStr("./fixtures/profile.html", { encoding: "utf8" });
     const actual = parseProfile(str);
-    assertEquals(actual, "abc");
+    assertEquals(actual, "/TravelMap-a_uid.41651DA6870F6D8783C64EEFC0D08986");
 });
--- api/parseProfile.ts
+++ api/parseProfile.ts
@@ -1,6 +1,6 @@
 const parseProfile = (str: string) => {
-  const key = '"/TravelMap-a_uid';
-  const re = new RegExp(`${key}(.*)"`,);
+  const key = '/TravelMap-a_uid';
+  const re = new RegExp(`${key}([^"]*)`,);
 
   const [firstMatched] = str.match(re) || [];
   return firstMatched;
