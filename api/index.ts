8c05cebbf0965f5be232a416da36139ff6af76e2
Nathan Rajlich <nathan@tootallnate.net>
2020-07-14 15:49:48 -0700

    Fixes for deploying to Vercel
    See explanation in https://github.com/TooTallNate/vercel-deno/issues/21.

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import { ServerRequest } from "https://deno.land/std@0.61.0/http/server.ts";
+import { getUrl } from "../src/url.ts";
+import { validate } from "../src/validate.ts";
+import { getMap } from "../src/map/index.ts";
+import { success, failure } from "../src/response.ts";
+
+export default async (req: ServerRequest) => {
+  try {
+    const url = getUrl(req);
+    validate(url);
+    const map = await getMap(url);
+    success(req, map);
+  } catch (error) {
+    failure(req, error.message);
+  }
+};
