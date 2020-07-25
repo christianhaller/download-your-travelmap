c44bc225346e0ffbdb8be4ff41bc4014c2c00f2d
Christian <hallo@christianhaller.com>
2020-07-25 23:37:04 +0200

    update deps

--- src/url.ts
+++ src/url.ts
@@ -1,4 +1,4 @@
-import type { ServerRequest } from "https://deno.land/std/http/server.ts";
+import type { ServerRequest } from "https://deno.land/std@0.62.0/http/server.ts";
 
 const getUrl = (req: ServerRequest): URL => {
   const [, query] = req.url.split("?");
