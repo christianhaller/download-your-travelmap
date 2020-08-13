fc0e773a733a349f1d9574d2722b078b935a0638
Christian <hallo@christianhaller.com>
2020-08-13 17:19:29 +0200

    wording

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -7,10 +7,9 @@ const fn = async (e) => {
   e.preventDefault();
   submitIcon.classList.add("hidden");
   spinnerIcon.classList.remove("hidden");
-
+  dl.classList.add("hidden");
   const url = input.value;
   const res = await fetch(`/api?url=${url}`);
-  const dl = form.nextElementSibling;
   spinnerIcon.classList.add("hidden");
   submitIcon.classList.remove("hidden");
   if (res.ok) {
@@ -28,5 +27,6 @@ const form = document.querySelector("form");
 const input = form.querySelector("#url") as HTMLInputElement;
 const submitIcon = form.querySelector(".js-submit-icon");
 const spinnerIcon = form.querySelector(".js-spinner");
+const dl = form.nextElementSibling;
 form.addEventListener("submit", fn);
 form.querySelector("button").classList.remove("cursor-not-allowed");
