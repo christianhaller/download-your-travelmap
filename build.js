ebdcdbf1b71831886b42e9c43d527c4691c87964
Christian <hallo@christianhaller.com>
2020-08-27 20:22:41 +0200

    fix deployment

--- build.js
+++ build.js
@@ -1,13 +1,27 @@
 const execa = require("execa");
 
 (async () => {
-  const res = await Promise.all([
-    execa("npm", ["run", "tailwind"]),
-    execa("npm", ["run", "postcss"]),
-    execa("npm", ["run", "rollup"]),
-    execa("npm", ["run", "templates"]),
-  ]);
-  res.forEach(({ stdout }) => {
-    console.log(stdout);
-  });
+  try {
+    const res = await Promise.all([
+      execa("npm", ["run", "tailwind"]),
+      execa("npm", ["run", "postcss"]),
+      execa("npm", ["run", "rollup"]),
+      execa("npm", ["run", "templates"]),
+    ]);
+    res.forEach(({ stdout }) => {
+      console.log(stdout);
+    });
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 })();
+
+process.on("unhandledRejection", function (reason, p) {
+  console.log(
+    "Possibly Unhandled Rejection at: Promise ",
+    p,
+    " reason: ",
+    reason
+  );
+});
