88065deeafa3b2bed7d5a2366dca918fa2c4397c
Christian Haller <hallo@christianhaller.com>
2020-10-29 22:07:42 +0100

    add highscore

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     content: [
       "./src/frontend/templates/*.html",
       "./src/frontend/response/success/Table*",
+      "./src/frontend/Highscore/Highscore.ts",
     ],
   },
   variants: {},
