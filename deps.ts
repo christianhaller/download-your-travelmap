adf2cf89167467a810cf2271cb98900e3bb8ac75
Christian Haller <hallo@christianhaller.com>
2020-11-20 21:10:17 +0100

    chore(app): update deps

--- deps.ts
+++ deps.ts
@@ -4,18 +4,18 @@ import {
   assertStrictEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.77.0/testing/asserts.ts";
+} from "https://deno.land/std@0.78.0/testing/asserts.ts";
 
 import {
   AWSSignerV4,
   Credentials,
 } from "https://deno.land/x/aws_sign_v4@0.1.4/mod.ts";
 
-import { createHash } from "https://deno.land/std@0.77.0/hash/mod.ts";
+import { createHash } from "https://deno.land/std@0.78.0/hash/mod.ts";
 
 import { spy, stub } from "https://deno.land/x/mock@v0.9.2/mod.ts";
-import * as log from "https://deno.land/std@0.77.0/log/mod.ts";
-import type { ServerRequest } from "https://deno.land/std@0.77.0/http/server.ts";
+import * as log from "https://deno.land/std@0.78.0/log/mod.ts";
+import type { ServerRequest } from "https://deno.land/std@0.78.0/http/server.ts";
 import "https://deno.land/x/dotenv@v1.0.1/load.ts";
 
 export {
