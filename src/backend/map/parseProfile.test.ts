c8115feda82f2e87b2baf6b64a168057b55dc2dc
Christian Haller <hallo@christianhaller.com>
2021-07-22 16:34:22 +0200

    user agent

--- src/backend/map/parseProfile.test.ts
+++ src/backend/map/parseProfile.test.ts
@@ -1,7 +1,7 @@
 import { parseProfile } from "./parseProfile.ts";
 import { assertEquals, assertThrows } from "../../../deps.ts";
 
-Deno.test("parseProfile", async () => {
+Deno.test("parseProfile1", async () => {
   const str = await Deno.readTextFile("./fixtures/profile.html");
   const actual = parseProfile(str);
   assertEquals(actual, "/TravelMap-a_uid.41651DA6870F6D8783C64EEFC0D08986");
@@ -15,6 +15,6 @@ Deno.test("parseProfile with exception", async () => {
       parseProfile(str);
     },
     Error,
-    "map link not found",
+    "map link not found"
   );
 });
