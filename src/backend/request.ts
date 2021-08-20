17791b096ddefeda1fc990535a633d935d5b44ae
Christian Haller <hallo@christianhaller.com>
2021-08-20 21:45:51 +0200

    ua

--- src/backend/request.ts
+++ src/backend/request.ts
@@ -10,6 +10,7 @@ const request = async (url: URL): Promise<string> => {
   const res = await fetch(href, {
     signal: c.signal,
     headers: {
+      "user-agent": userAgent,
       accept:
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
