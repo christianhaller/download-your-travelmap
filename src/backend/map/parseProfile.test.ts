60699cc3de1f377789a6cbd9a1441f22c4b0c671
Christian Haller <hallo@christianhaller.com>
2021-12-14 12:18:52 +0100

    fix profile parsing

--- src/backend/map/parseProfile.test.ts
+++ src/backend/map/parseProfile.test.ts
@@ -7,6 +7,13 @@ Deno.test("parseProfile1", async () => {
   assertEquals(actual, "/TravelMap-a_uid.41651DA6870F6D8783C64EEFC0D08986");
 });
 
+
+Deno.test("parseProfile2", async () => {
+  const str = await Deno.readTextFile("./fixtures/profile2.html");
+  const actual = parseProfile(str);
+  assertEquals(actual, "/TravelMap-a_uid.0C6BE52F6C7829963646D499A74E256F");
+});
+
 Deno.test("parseProfile with exception", async () => {
   const str = await Deno.readTextFile("./fixtures/homepage.html");
 
